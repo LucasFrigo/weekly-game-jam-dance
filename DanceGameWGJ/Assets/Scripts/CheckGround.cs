f1d44f0b498cfe77c7ffd98a2b0dd5fb80dff31b
Lucas Guilherme <lukeg299@gmail.com>
2020-04-19 18:04:25 -0300

    Implemented basic character controller

---
+++ DanceGameWGJ/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    GameObject Player;
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerMovement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerMovement>().isGrounded = false;
+        }
+    }
+}
